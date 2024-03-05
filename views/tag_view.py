31296255dc3db2402b1c2a0ff46931aa6f6472a2
Will MacLean <maclean.will@gmail.com>
2024-03-05 15:04:49 -0600

    refactored functions

--- views/tag_view.py
+++ views/tag_view.py
@@ -4,7 +4,6 @@ import json
 
 def create_tag(tag):
     with sqlite3.connect("./db.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute(
@@ -34,7 +33,6 @@ def delete_a_tag(pk):
 
 def edit_tag(tag_data, pk):
     with sqlite3.connect("./db.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute(
