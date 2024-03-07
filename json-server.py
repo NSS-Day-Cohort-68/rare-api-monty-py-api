7ae39e1c8a5851a628068daaee67cd2a225d63bd
Aubry Freeman <aubryfreeman23@gmail.com>
2024-03-07 15:30:42 -0600

    Created create_post, post method for creating post

--- json-server.py
+++ json-server.py
@@ -2,7 +2,7 @@ import json
 from http.server import HTTPServer
 from monty_handler import HandleRequests, status
 
-from views import create_tag, create_category, create_post_tag, create_user
+from views import create_tag, create_category, create_post_tag, create_user, create_post
 from views import get_user_posts, get_all_posts
 
 
@@ -24,10 +24,9 @@ class JSONServer(HandleRequests):
             if "user_id" in url["query_params"]:
                 response_body = get_user_posts(url["query_params"]["user_id"][0])
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
-            
+
             response_body = get_all_posts()
             return self.response(response_body, status.HTTP_200_SUCCESS.value)
-        
 
     def do_POST(self):
         """Handle POST requests from a client"""
@@ -98,7 +97,23 @@ class JSONServer(HandleRequests):
                     "Please fill out the required fields",
                     status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
                 )
-         # If true/false... More detailed responses for failing to create user
+        elif requested_resource == "posts":
+            post_data = json.loads(request_body)
+
+            new_post_id = create_post(post_data)
+
+            if new_post_id:
+                response_body = {"id": new_post_id}
+                return self.response(
+                    json.dumps(response_body), status.HTTP_201_SUCCESS_CREATED.value
+                )
+            else:
+                return self.response(
+                    "Please fill out the required fields",
+                    status.HTTP_400_CLIENT_ERROR_BAD_REQUEST_DATA.value,
+                )
+        # If true/false... More detailed responses for failing to create user
+
 
 def main():
     host = ""
