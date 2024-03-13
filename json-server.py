639dd6137426c4c22423f8837065de422c8efa18
Eric Steedly <ericsteedly@gmail.com>
2024-03-13 17:07:57 -0500

    updated post/create category response

--- json-server.py
+++ json-server.py
@@ -142,12 +142,11 @@ class JSONServer(HandleRequests):
         elif requested_resource == "categories":
             category_data = json.loads(request_body)
 
-            new_category_id = create_category(category_data)
+            new_category = create_category(category_data)
 
-            if new_category_id:
-                response_body = {"id": new_category_id}
+            if new_category:
                 return self.response(
-                    json.dumps(response_body), status.HTTP_201_SUCCESS_CREATED.value
+                    "", status.HTTP_201_SUCCESS_CREATED.value
                 )
             else:
                 return self.response(
