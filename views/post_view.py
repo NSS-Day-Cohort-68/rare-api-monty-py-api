5eeec7c48e4e7a845056acf2ce4dfae435e351dd
Rachel Kendricks <rachelanne.kendricks@gmail.com>
2024-03-14 14:33:29 -0500

    added descending order in SQL query for dates

--- views/post_view.py
+++ views/post_view.py
@@ -24,6 +24,7 @@ def get_all_posts():
     FROM Posts p
     JOIN Users u ON u.id = p.user_id
     JOIN Categories c ON c.id = p.category_id
+    ORDER BY p.publication_date DESC
     """
     )
     posts = []
